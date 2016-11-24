515a0058db42e90244d34289ea080d1095b18d78
kade-robertson <kade.robertson96@gmail.com>
2016-11-24 08:57:06 -0500

    read and parse tokens

---
+++ tone-deaf.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+
+def process(tokens):
+    stack = []
+    pass
+
+def readf(path, lyrics):
+    prog = ''
+    with open(path, 'r') as file:
+        prog = file.read()
+    prog = prog.split('\n')
+    if lyrics: prog = prog[::2]
+    prog = [x.split() for x in prog]
+    tokens = []
+    for row in prog:
+        tokens += row
+    process(tokens)
+    
+
+def main():
+    parser = argparse.ArgumentParser(description = 'Interpreter for the tone-deaf programming language.')
+    parser.add_argument('-f', type=str, help='Open a tone-deaf source file.')
+    parser.add_argument('-l', action='store_true', help='Open the source file in "lyric mode".')
+    args = parser.parse_args()
+    if args.f != None:
+        if args.f != '':
+            if os.path.isfile(args.f):
+                readf(args.f, args.l)
+            else:
+                print('-f: Error - no path given.')
+        else:
+            print('-f: Error - no path given.')
+        
+
+if __name__ == '__main__':
+    main()
